0108e234855f800540e2467817cbc5e7c0aa7031
Yvan J. Aquino <yvan.aquino@cloud-colosseum.net>
2022-03-30 16:36:22 +0000

    Updates to chat models

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+# Copyright 2022 Google LLC
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from fastapi import FastAPI
 
 from modules.models import WebhookRequest
@@ -5,25 +19,26 @@ from modules.whr_client import WebhookResponse
 from modules.matcher import NameMatcher
 
 matcher = NameMatcher(source_file='data/full_names.json')
-# matcher = NameMatcher.from_file('ai-models/matcher.mdl')
 
 app = FastAPI()
 
+
 @app.post('/retry-full-name')
 async def name_provided(webhook: WebhookRequest):
+    # Get session parameters, get the retry_full_name, and prepare a response.
     parameters = webhook.sessionInfo.parameters
+    name = parameters.get('retry_full_name')
     response = WebhookResponse()
 
-    name = parameters.get('retry_full_name')
-    print("name:", name)
+    # If the name is missing, return a text response indicating not retry name
+    # was not found within the session parameters.
     if not name:
         response.add_text_response('retry_full_name was not found within the session parameters.')
-
     else:
         matches = matcher.match(name)
-        best_match = matches[0]
-        bast_match_name = best_match['source']
-        response.add_text_response(f'Okay. I heard {bast_match_name}. Did I get it right this time?')
+        best_match = matches[0] # matches are sorted by scoring.
+        best_match_name = best_match['source']
+        response.add_text_response(f'Okay. I heard {best_match_name}. Did I get it right this time?')
 
     return response
 
--- modules/stringpipe.py
+++ modules/stringpipe.py
@@ -1,3 +1,17 @@
+# Copyright 2022 Google LLC
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import string
 
 
@@ -8,7 +22,9 @@ class StringOperator:
         self.args = args
         self.kwargs = kwargs
 
+    # do calls the functions on either the string or each string element of a given list.
     def do(self, inp):
+        
         if isinstance(inp, str):
             return self.func(inp, *self.args, **self.kwargs)
         elif isinstance(inp, list):
@@ -57,7 +73,7 @@ class StringOperator:
         return cls(StringOperator.to_cum_ngrams, n, m) 
 
 
-class StringPipeline:
+class StringPipeline:   
 
     def __init__(self):
         self.ops = []
